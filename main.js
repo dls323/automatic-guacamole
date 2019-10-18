03b1036a2e9a9bed87cb02818e422d776c49cb94
dls323 <55297586+dls323@users.noreply.github.com>
2019-10-17 21:43:49 -0500

    Update main.js
    new changes and there something not allowing me to add to summary

--- main.js
+++ main.js
@@ -74,16 +74,23 @@ var userInfo = {
 
 // Shopping cart info and calculations
 var cart = {
-    cartAmt: 0, 
-    taxAmt: 0, 
-    shipAmt: 0, 
+    cartAmt: 0,
+    taxAmt: 0,
+    shipAmt: 0,
     totalAmt: 0,
-    calc: function(qty, price) { 
+    calc: function(qty, price) {
         this.cartAmt = qty * price;
         this.taxAmt = this.cartAmt * 0.08 * 1.0;
         this.shipAmt = this.cartAmt * 0.03;
         this.totalAmt = this.cartAmt + this.taxAmt + this.shipAmt;
     }
+}
+// Derek stuff
+var cartSum = {
+    cartSumAmt: "",
+    cartTax: "",
+    cartShip: "",
+    cartTotal: "",
 }
 
 var iframe, partSelect, qtySelect, qty, price, unitPrice, totalPrice, physicalAddr;
@@ -92,35 +99,35 @@ var iframe, partSelect, qtySelect, qty, price, unitPrice, totalPrice, physicalAd
 document.addEventListener('DOMContentLoaded', function(){
     loadIframeContents();
     iframe.addEventListener('load', loadIframeContents);
-    
+
 });
 
 
-// Loads iframe contents into object 
+// Loads iframe contents into object
 function loadIframeContents(){
-    
+
     // Reset values
-    var submitButton = null; 
+    var submitButton = null;
     var submitFunc = null;
-    var form = null; 
-    
+    var form = null;
+
     // Grab new page in iframe
     iframe = document.getElementById("main-frm");
-    
+
     // Sets up functions and listeners for pages
-    
+
     // SHOPPING CART
     if(!submitButton){
-        
+
         submitButton = iframe.contentWindow.document.getElementById("cart-submit");
         submitFunc = cartSubmit;
-        
+
         // Populate cart details when a new unit size or product is select
         qtySelect = iframe.contentWindow.document.querySelector('[name="quantity"]');
         if(qtySelect){
             qtySelect.addEventListener('input', populateCart);
         }
-        
+
         // Reset quanitity to 1 when changing products
         partSelect = iframe.contentWindow.document.querySelector('[name="parts"]');
         if(partSelect){
@@ -129,14 +136,14 @@ function loadIframeContents(){
                 populateCart();
             });
         }
-        
+
     }
     // USER INFO
     if(!submitButton){
-        
+
         submitButton = iframe.contentWindow.document.getElementById("user-info-submit");
         submitFunc = userInfoSubmit;
-        
+
         // Populate user info with saved values if there is any
         if(submitButton){
             iframe.contentWindow.document.querySelector('[name="fullname"]').value = userInfo.name;
@@ -149,31 +156,32 @@ function loadIframeContents(){
     }
     // SHIPPING INFO
     if(!submitButton){
-        
+
         submitButton = iframe.contentWindow.document.getElementById("ship-submit");
         submitFunc = shipSubmit;
         physicalAddr = iframe.contentWindow.document.getElementById("physical-address");
-        
+
         // Populate address info with physical address in user info if checkbox is checked
         if(submitButton){
             physicalAddr.addEventListener("change", usePhysicalAddr);
         }
     }
-    // CHECKOUT 
+    // CHECKOUT
     if(!submitButton){
-        
+
         submitButton = iframe.contentWindow.document.getElementById("checkout-submit");
         submitFunc = checkoutSubmit;
-        
+
         // Populate checkout values using values already calculated in shopping cart
         if(submitButton){
             iframe.contentWindow.document.getElementById("cart-amount").value = "$" + cart.cartAmt.toFixed(2);
             iframe.contentWindow.document.getElementById("tax-amount").value = "$" + cart.taxAmt.toFixed(2);
             iframe.contentWindow.document.getElementById("ship-amount").value = "$" + cart.shipAmt.toFixed(2);
             iframe.contentWindow.document.getElementById("total-amount").value = "$" + cart.totalAmt.toFixed(2);
+            iframe.contentWindow.document.getElementById("supplyAmt").value = "$" + cartSum.cartSumAmt.toFixed(2);// Derek stuff
         }
     }
-    
+
     // Grabs form element of iframe page and changes submit button function
     if(submitButton){
         form = iframe.contentWindow.document.querySelector("form");
@@ -182,7 +190,7 @@ function loadIframeContents(){
 }
 
 function populateCart() {
-    
+
     unitPrice = iframe.contentWindow.document.querySelector('#unit-price');
     totalPrice = iframe.contentWindow.document.querySelector('#total-price');
     qty = qtySelect.value;
@@ -198,17 +206,36 @@ function populateCart() {
         totalPrice.value = "";
         unitPrice.value = "";
     }
-    
+
+}
+
+function populateSummary() {
+
+    totalPrice = iframe.contentWindow.document.querySelector('#total-price');
+    qty = qtySelect.value;
+    qty = qty * 1.0;
+    // Check object for given name and then populate fields based on product info
+    for(var i=0; i<items.length; i++){
+        if(items[i].title == partSelect.value){
+            price = items[i].price.toFixed(2);
+            totalPrice.value = "$" + (price * qty).toFixed(2);
+            unitPrice.value = "$" + price;
+            break;
+        }
+        totalPrice.value = "";
+        unitPrice.value = "";
+    }
+
 }
 
 
-function cartSubmit(){    
+function cartSubmit(){
     cart.calc(qty, price);
     console.log(cart);
 }
 
 function userInfoSubmit(){
-    
+
     // Save user info in object
     userInfo.name = iframe.contentWindow.document.querySelector('[name="fullname"]').value;
     userInfo.addr[0] = iframe.contentWindow.document.querySelector('[name="addr1"]').value;
@@ -216,7 +243,7 @@ function userInfoSubmit(){
     userInfo.city = iframe.contentWindow.document.querySelector('[name="city"]').value;
     userInfo.state = iframe.contentWindow.document.querySelector('[name="state"]').value;
     userInfo.zip = iframe.contentWindow.document.querySelector('[name="zip"]').value;
-    
+
 }
 
 
@@ -240,9 +267,19 @@ function usePhysicalAddr(){
 }
 
 function shipSubmit(){
-    
+
 }
 
 function checkoutSubmit(){
-    
+    cart.cartAmt = iframe.contentWindow.document.getElementById("cart-amount").value; // Derek stuff
+    cart.taxAmt = iframe.contentWindow.document.querySelector('[name="taxAmt"]').value;// Derek stuff
+    cart.shipAmt = iframe.contentWindow.document.querySelector('[name="shipAmt"]').value;// Derek stuff
+    cart.totalAmt = iframe.contentWindow.document.querySelector('[name="totalAmt"]').value;// Derek stuff
+}
+
+function summaryInfo(){
+    iframe.contentWindow.document.querySelector('[name="supplyAmt"]').value = cart.cartAmt;// Derek stuff
+    iframe.contentWindow.document.querySelector('[name="taxAmt"]').value = cart.taxAmt;// Derek stuff
+    iframe.contentWindow.document.querySelector('[name="shipFee"]').value = cart.shipAmt;// Derek stuff
+    iframe.contentWindow.document.querySelector('[name="totalAmt"]').value = cart.totalAmt;// Derek stuff
 }
